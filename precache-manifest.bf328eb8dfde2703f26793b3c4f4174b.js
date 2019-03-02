f33dbcc8e1388708678dc1292c786ed6cf2ab029
Joanna Krezolek <j.krezolek@gmail.com>
2019-03-02 18:04:51 +0100

    Updates

---
+++ precache-manifest.bf328eb8dfde2703f26793b3c4f4174b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baeea33d2ac2643f9a5f",
+    "url": "/todo_MDB/static/js/runtime~main.baeea33d.js"
+  },
+  {
+    "revision": "e12b0143bdc9f1d06782",
+    "url": "/todo_MDB/static/js/main.e12b0143.chunk.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/todo_MDB/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "e12b0143bdc9f1d06782",
+    "url": "/todo_MDB/static/css/main.221102e9.chunk.css"
+  },
+  {
+    "revision": "e823890e5ae693dc29374d1526b9a1b5",
+    "url": "/todo_MDB/index.html"
+  }
+];
